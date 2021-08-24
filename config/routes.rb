f355d49859094bbf5ae08d2c0ded95493dbcbeb4
Adam Ringrose <whatsyerpoison1@gmail.com>
2021-08-24 16:02:25 +0100

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pets#index'
-  resources :pets
+  resources :pets do
+    resources :bookings, except: [:index, :destroy]
+  end
+  resources :bookings, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
