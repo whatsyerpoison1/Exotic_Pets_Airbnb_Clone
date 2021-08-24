0f61f56621ddb3149b200db344a5ada9fde8a3b3
jonathanberry <jonathanberry45@gmail.com>
2021-08-24 12:49:35 +0100

    pets controller added

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,33 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
+private
+
+  def pet_params
+    params.require(:pet).permit(:name, :type, :description)
+  end
 end
